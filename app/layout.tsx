8e38feff092130d2168928880c93798fe23253cd
GKarachalios <137404788+GKarachalios@users.noreply.github.com>
2023-11-28 13:14:24 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "TraveLovers",
+  description: "Travel guide for camping!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
